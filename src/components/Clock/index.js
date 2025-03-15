e9bea9b39f3eae85ecca250371ebaa9abd369ecd
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2025-03-15 18:15:50 +0100

    Clock.

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,68 +1,60 @@
-import { number } from "prop-types";
 import css from "./Clock.module.css";
 import { useState } from "react";
 export const Clock = () => {
 
     const [hour, setHour] = useState(0);
 
-    const hourRotation = (hour / 12) * 360;
-
-    const handleMouseMove = (event) => {
-        const rect = event.target.getBoundingClientRect();
+    const handleClickSetTime = (e) => {
+        const rect = e.target.getBoundingClientRect();
         const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const mouseX = event.clientX - rect.left;
-        const mouseY = event.clientY - rect.top;
+        const centerY = rect.height / 2; 
 
-        const distanceFromCenter = Math.sqrt(Math.pow(mouseX - centerX, 2) + Math.pow(mouseY - centerY, 2))
+        const clickX = e.clientX - rect.left;
+        const clickY = e.clientY - rect.top;
 
-        if( distanceFromCenter < 25){
-            return ;
-        }
+        let angle = Math.atan2(clickY - centerY, clickX - centerX) * (180 / Math.PI)
+        console.log(angle + 90)
 
-        const angle = Math.atan2(mouseY - centerY, mouseX - centerX) * (180 / Math.PI);
-        let correctedAngle = angle < 0 ?  angle + 360 : angle;
-
-
-        const newHour = (correctedAngle + 90 ) % 360 / 30 % 12;
+        const newHour = Math.floor(angle/30) || 12;
 
         setHour(newHour);
     }
 
-        const numbers = Array.from({length : 12}, (_, i) => {
-            const angle = ((i + 1) / 12) * 2 * Math.PI;
-            const radius = 50;
-            const x  = radius + 40 * Math.cos(angle - Math.PI / 2);
-            const y = radius + 40 * Math.sin(angle - Math.PI / 2);
-            return(
-                <g key={i} className={css.clockNumberGroup}>
-                    <circle cx={x} cy={y} r="10"fill="transparent" className={css.hoverCircle}/>
-                    <text
-                        className={css.clockNumber}
-                        key={i}
-                        x={x}
-                        y={y}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                        fontSize="6"
-                        fill="black">
-                            {i + 1}
-                    </text>
-                </g>
-            )
+    const renderClockNumbers = () => {
+        let numbers = [];
+        for (let i = 1; i <= 12 ; i++){
+            const angle = (i - 3) * 30;
 
-        })
+            const x = Math.cos((angle * Math.PI) / 180) * 110 + 150;
+            const y = Math.sin((angle * Math.PI) / 180) * 110 + 150;
+            if(i === 3) {
+                console.log(y)
+            }
+            
+
+
+            numbers.push(
+                <div
+                key={i}
+                className={css.clockNumber}
+                style={{top:`${y}px`, left:`${x}px`}}
+                >
+                    {i}
+                </div>
+            )
+        }
+        return numbers
+    }
+
 
     return(
-        <div className={css.clock} onMouseMove={handleMouseMove}>
-            <svg className="hour-line" viewBox="0 0 100 100">
-                {numbers}
-                <line x1="50" y1="50" 
-                x2="50" y2="10" 
-                stroke="black" 
-                strokeWidth="2"
-                transform={`rotate(${hourRotation} 50 50)`}/>
-            </svg>
+        <>
+        <div className={css.clock} onClick={handleClickSetTime}>
+            <line
+            x1="50"y1="50"
+            x2="50"y2="10"></line>
+            {renderClockNumbers()}
         </div>
+        </>
     )
 }
